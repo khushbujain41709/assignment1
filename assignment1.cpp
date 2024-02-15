4d648808e01b67d125b2c48114940f0fad6da41c
Khushbu Jain <149000531+khushbujain41709@users.noreply.github.com>
2024-02-15 12:21:51 +0530

    Add files via upload
    This is my first assignment od module basics of c++
    Owner : Khushbu Jain

---
+++ assignment1.cpp
@@ -0,0 +1,32 @@
+//QUESTION 1 
+# include<iostream>
+using namespace std;
+int main(){
+    cout << "This assignment is done by Khushbu Jain."<<endl;
+    cout<<"Physics"<<endl<<"Wallah"<<endl;
+
+
+//QUESTION 2 
+    cout<<"output of question 2 is : "<<5+5<<endl;
+
+
+//QUESTION 3
+    cout<<"Space taken by int is : " <<sizeof(int)<<"bytes"<<endl;
+    cout<<"Space taken by bool is : " <<sizeof(bool)<<"bytes"<<endl;
+    cout<<"Space taken by float is : " <<sizeof(float)<<"bytes"<<endl;
+    
+    
+//QUESTION 4
+    cout<<"output of question 4 without code is : 6 4"<<endl;
+
+    
+//QUESTION 5
+    int radius = 10;
+    int circumference = 3.14 *radius *radius;
+    cout<<"Circumference od a circle with radius 10 is :"<< circumference<<endl;
+
+//QUESTION 6
+    cout<<"only _FLOAT and FLOAT are the valid variable names."<<endl;
+
+    return 0 ;
+}
